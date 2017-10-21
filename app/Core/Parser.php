97125dc63167c87bbfcd1677d215bb395e3224eb
Kirill.P <escape6661@gmail.com>
2017-10-21 16:35:28 +0700

    init

---
+++ app/Core/Parser.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ONLY. Digital Agency | Kirill
+ * Date: 21/10/2017
+ * Time: 16:25
+ */
+
+namespace App\Core;
+
+abstract class Parser {
+
+}
---
+++ app/Http/Controllers/DromController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DromController extends Controller
+{
+    public function parse() {
+        return 'hello';
+    }
+}
